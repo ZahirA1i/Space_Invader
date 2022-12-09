3b99745cf7cfa3ebb29a1128e2b188f630cedcb6
Zahir Ali <zahirali@Zahirs-Air.vc.shawcable.net>
2022-12-09 05:46:43 -0800

    finalized invader hit marker

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ class Projectile {
         this.position = position
         this.velocity = velocity
 
-        this.radius = 3
+        this.radius = 4
     }
 
     draw() {
@@ -215,8 +215,30 @@ function animate() {
 
     grids.forEach(grid => {
         grid.update()
-        grid.invaders.forEach(invader => {
+        grid.invaders.forEach((invader, i) => {
             invader.update({velocity: grid.velocity})
+
+            projectiles.forEach((projectile, j) => {
+                if (projectile.position.y - projectile.radius <= invader.position.y + invader.height && 
+                    projectile.position.x + projectile.radius >= invader.position.x && projectile.position.x - 
+                    projectile.radius <= invader.position.x + invader.width && projectile.position.y + projectile.radius >= invader.position.y
+                    
+                    ) {
+
+                    setTimeout(() => {
+                        const invaderFound = grid.invaders.find((invader2) => 
+                             invader2 === invader
+                        )
+                        const projectileFound = projectiles.find(projectile2 => projectile2 === projectile)
+
+                        if (invaderFound && projectileFound) {
+                            grid.invaders.splice(i, 1)
+                            projectiles.splice(j,1)
+                        }
+
+                    }, 0)
+                }
+            })
         })
     })
 
