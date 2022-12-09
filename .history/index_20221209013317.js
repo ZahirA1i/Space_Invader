7dad397c67ace58b36bcadb7c5e92626895d990e
Zahir Ali <zahirali@Zahirs-Air.vc.shawcable.net>
2022-12-09 01:39:16 -0800

    player base made

---
+++ .history/index_20221209013317.js
@@ -0,0 +1,16 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Player {
+    constructor(){
+        this.position = {
+            x: 200,
+            y: 200
+        }
+
+    }
+}
+
