9a916b2d7a0e139c80ba6769748757a49dae7b98
Zahir Ali <zahirali@Zahirs-Air.vc.shawcable.net>
2022-12-09 08:16:36 -0800

    added player explosion

--- index.js
+++ index.js
@@ -90,20 +90,26 @@ class Particle {
 
         this.radius = radius
         this.color = color
+        this.opacity = 1
     }
 
     draw() {
+        c.save()
+        c.globalAlpha = this.opacity
         c.beginPath()
         c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
         c.fillStyle = this.color
         c.fill()
         c.closePath()
+        c.restore()
     }
 
     update() {
         this.draw()
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
+
+        this.opacity -= 0.01
     }
 }
 
@@ -259,6 +265,24 @@ const keys = {
 let frames = 0
 let randomInterval = Math.floor(Math.random() * 500 + 500)
 
+function createParticles({object, color}){
+    for (let i = 0; i < 15; i++) {
+        particles.push(
+            new Particle({
+            position: {
+                x: object.position.x + object.width / 2,
+                y: object.position.y + object.height / 2
+            },
+            velocity: { 
+                x: (Math.random() - 0.5) * 2,
+                y: (Math.random() - 0.5) * 2
+            },
+            radius: Math.random() * 3,
+            color: color || '#baa0de'
+        }))
+    }
+}
+
 console.log(randomInterval)
 
 function animate() {
@@ -266,9 +290,18 @@ function animate() {
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
-    particles.forEach(particle => {
-        particle.update()
+    particles.forEach((particle, i) => {
+        if(particle.opacity <= 0) {
+            setTimeout(() => {
+                particles.splice(i, 1)
+            }, 0)
+        } else {
+            particle.update()
+        }
     })
+
+        console.log(particles)
+
     invaderProjectiles.forEach((invaderProjectile, index) => {
     if (invaderProjectile.position.y + invaderProjectile.height >= canvas.height){
         setTimeout(() => {
@@ -276,13 +309,22 @@ function animate() {
         }, 0)
     } else invaderProjectile.update()
 
+    //player explosion
+
         if (invaderProjectile.position.y + 
             invaderProjectile.height >= player.position.y && invaderProjectile.position.x + 
             invaderProjectile.width >= player.position.x && invaderProjectile.position.x <= player.position.x + 
             player.width
             
             ) {
+                setTimeout(() => {
+                    invaderProjectiles.splice(index, 1)
+                }, 0)
                     console.log ('you lose')
+                    createParticles({
+                        object: player,
+                        color: 'white'
+                    })
         }
     })
 
@@ -314,19 +356,7 @@ function animate() {
                     projectile.radius <= invader.position.x + invader.width && projectile.position.y + projectile.radius >= invader.position.y
                     
                     ) {
-                        particles.push(new Particle({
-                            position: {
-                                x: invader.position.x + invader.width / 2,
-                                y: invader.position.y + invader.height / 2
-                            },
-                            velocity:{
-                                x: 2,
-                                y: 2
-                            },
-                            radius: 10,
-                            color: 'yellow'
-                        }))
-
+                        
 
                     setTimeout(() => {
                         const invaderFound = grid.invaders.find((invader2) => 
@@ -336,6 +366,11 @@ function animate() {
 
                         //remove invader and projectile
                         if (invaderFound && projectileFound) {
+                            createParticles({
+                                object: invader,
+                            })
+
+
                             grid.invaders.splice(i, 1)
                             projectiles.splice(j,1)
 
